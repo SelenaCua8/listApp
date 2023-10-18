1fbf3a0e9500ec6d49cf3269a04842f81de91695
Selena <Selenac99@hotmail.com>
2023-10-18 10:02:45 -0300

    editando components

--- my-list-app/src/app/components/item/item.component.ts
+++ my-list-app/src/app/components/item/item.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  
 })
 export class ItemComponent {
 
