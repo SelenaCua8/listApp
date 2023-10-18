1346de0957a0cca3a28789b264a4b14b38f41446
Selena <Selenac99@hotmail.com>
2023-10-18 10:27:07 -0300

    me da errores jeje

--- my-list-app/src/app/services/item.service.ts
+++ my-list-app/src/app/services/item.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  id: number;
+  itemSub: TaskModel[] = [];
+  private sub = new Subject<TaskModel[]>();
+  item$ = this.sub.asObservable();
+
+  constructor() {}
+
+  private generateId() {
+    return '-' + Math.random().toString(36).substring(2, 9);
+  }
+
+  add(newItem: TaskModel){
+    this.get();
+    newItem.id = this.generateId();
+    this.itemSub.push(newItem);
+    this.sub.next(this.itemSub);
+    localStorage.setItem('item', JSON.stringify(this.itemSub));
+
+  }
+
+  get(){
+    let listItems = JSON.parse(localStorage.getItem('item'));
+    if(listItems == null){
+      this.itemSub = [];
+      this.sub.next{[]};
+    }else{
+      this.itemSub = listItems;
+      this.sub.next(listItems);
+    }
+  }
+
 }
