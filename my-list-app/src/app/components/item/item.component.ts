3867895bb664e45b53761e865f24e8311d76ffd2
Selena <Selenac99@hotmail.com>
2023-10-18 11:35:41 -0300

    terminando la lista de tareas

--- my-list-app/src/app/components/item/item.component.ts
+++ my-list-app/src/app/components/item/item.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ItemService } from 'src/app/services/item.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  
+  styles: [
+  ]
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit {
+
+  @Input() item: any;
+  @Input() index: any;
+  chkItems: FormControl = new FormControl();
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.chkItems = new FormControl(this.item.state);
+    this.chkItems.valueChanges.subscribe(chkState => {
+        this.changeState(chkState);
+    })
+  }
+
+  deleteItem(itemId: string){
+    this.itemService.delete(itemId);
+  }
+
+  changeState(chkState: boolean){
+    this.itemService.changeState(this.item.id, chkState);
+  }
 
 }
