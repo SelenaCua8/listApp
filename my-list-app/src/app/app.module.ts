f05f5e9ddedcbe0d6f241c0ae0e3c5c8c8f517e5
Selena <Selenac99@hotmail.com>
2023-10-18 09:53:40 -0300

    generando carpetas nuevas

--- my-list-app/src/app/app.module.ts
+++ my-list-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemComponent } from './components/item/item.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ItemComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
